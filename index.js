02c67fb4a6fa6295cac004939e844b1cfcfdf954
norato777 <norato777@gmail.com>
2022-11-20 14:41:22 -0600

    updated function pandora

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-function laCajaDePandora(numero) {
-  // proximamente escribiremos codigo aqui
+function laCajaDePandora(num) {
+  if (num % 2 === 0) {
+    let dec = [];
+    if (num <= 0) return false;
+    while (num > 0) {
+      dec.push(num % 2);
+      num = Math.floor(num / 2);
+    }
+    return dec.reverse().join("");
+  }
 }
+
+console.log(laCajaDePandora(2));
+console.log(laCajaDePandora(14));
 
 function edd() {
   class obj {
