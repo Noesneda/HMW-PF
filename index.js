d141f5d5455d78bbc30ebc2bc1e8e4c9bd58571d
Andres <andrescantlon@gmail.com>
2022-11-20 17:25:26 -0300

    Andy commit

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+function laCajaDePandora(numero) {
+    if (numero % 2 === 0) {
+      let binario = numero.toString(2);
+      return binario;
+    } else {
+      let hexadecimal = numero.toString(16);
+      return hexadecimal;
     }
+  }
+  
+  function andres() {
+    class objeto {
+      constructor(nombre, edad, nacionalidad) {
+        nombre = Andy,
+        edad = 38,
+        nacionalidad = argentino
+      }
+    }
+    return objeto;
+  }
