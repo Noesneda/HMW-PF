db665cd1dc84123482851b795d844acdd95e6617
norato777 <norato777@gmail.com>
2022-11-20 14:34:48 -0600

    function 2

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+function laCajaDePandora(numero) {
+  // proximamente escribiremos codigo aqui
+}
+
+function edd() {
+  class obj {
+    constructor(name, age, nationality) {
+      (name = "Edd"), (age = 45), (nationality = "Colombian");
     }
+  }
+  return obj;
+}
