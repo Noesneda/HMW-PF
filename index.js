d7391d62dbb19cb3c59600f05a8a8de781669822
NachoAbait <102827148+NachoAbait@users.noreply.github.com>
2022-11-20 15:46:44 -0300

    funcion llenita

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+function laCajaDePandora(num) {
+  if (num % 2 === 0) {
+    let number = num;
+    let binary = (num % 2).toString();
+    for (; num > 1; ) {
+      number = num / 2;
+      binary = (num % 2) + binary;
     }
+    return binary;
+  } else {
+    let mensajito = "No lo pude hacer. sorrynotsorry";
+    return mensajito;
+  }
+}
