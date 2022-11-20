8c65c9b5552781592406192a1eb1bb1a4c8d1046
khala87 <fragiel87@gmail.com>
2022-11-20 17:53:41 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-function laCajaDePandora(numeros){
+function laCajaDePandora(numero){
     // proximamente escribiremos codigo aqui
-
+    if(numero % 2 === 0){
+        return (numero).toString(2)
+    }else{
+        return numero.toString(16)
+    }
     }
