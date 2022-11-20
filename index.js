af476449cfded9c7d86eec2f6b653fc46f64db87
Alejandra Aragon <alejita.0418@gmail.com>
2022-11-20 17:37:32 -0300

    funcion ale

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+   if (numero % 2 === 0) {
+      let n = numero;
+      let b = (numero % 2).toString();
+   while(numero > 0) {
+     n = (numero / 2);
+     b = (num % 2) + b; 
     }
+     return b;
+   } else {
+      let hexadecimal = "de lo contrario hexadecimal"
+      return hexadecimal;
+   } 
+    }
+
+
+
+
+   let Alejandra = () => {
+      let obj = {
+         nombre: "Alejandra",
+         edad: 30,
+         nacionalidad: "Colombiana"
+      }
+      return obj;
+   }
+
+    
